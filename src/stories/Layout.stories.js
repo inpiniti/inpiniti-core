27406b10e15739c90c78d5ea1843bbc5ad1a2df8
youngkyunjung <younginpiniti@gmail.com>
2023-06-12 06:54:20 +0900

    layout 컴포넌트 추가

---
+++ src/stories/Layout.stories.js
@@ -0,0 +1,13 @@
+import { Layout } from "./Layout";
+import { Header } from "./Header";
+
+import { LoggedIn } from "./Header.stories";
+
+export default {
+  title: "Layout",
+  component: Layout,
+};
+
+export const Empty = {
+  args: {},
+};
