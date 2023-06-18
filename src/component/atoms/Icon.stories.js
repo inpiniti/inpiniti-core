28354dd63a1450d35076a1a311f5d1a05244d7b6
youngkyunjung <younginpiniti@gmail.com>
2023-06-19 00:04:23 +0900

    아이콘 버튼 추가

---
+++ src/component/atoms/Icon.stories.js
@@ -0,0 +1,12 @@
+import { Icon } from "./Icon";
+
+export default {
+  title: "atoms/Icon",
+  component: Icon,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+export const IconDefault = {
+  args: {},
+};
