029213e3ebe1606cd0b68278dadbe393c407dc89
ygjung <ygjung@iochord.com>
2023-06-23 14:36:17 +0900

    npm 배포를 위한 작업

---
+++ inpiniti-core-module.js
@@ -0,0 +1,19 @@
+import Button from './src/component/atoms/Button';
+import Footer from './src/component/atoms/Footer';
+import Icon from './src/component/atoms/Icon';
+import Main from './src/component/atoms/Main';
+import Aside from './src/component/molecules/Aside';
+import Content from './src/component/molecules/Content';
+import Header from './src/component/molecules/Header';
+import Layout from './src/component/organisms/Layout';
+
+export {
+	Button,
+	Footer,
+	Icon,
+	Main,
+	Aside,
+	Content,
+	Header,
+	Layout,
+}
