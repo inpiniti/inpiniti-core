936a0990edf1d27e234a3e723fa937ce05c03d95
youngkyunjung <younginpiniti@gmail.com>
2023-06-24 16:12:08 +0900

    header2 and icon 추가

---
+++ src/component/molecules/Header2.stories.jsx
@@ -0,0 +1,12 @@
+import { Header2 } from "./Header2";
+
+export default {
+  title: "molecules/Header2",
+  component: Header2,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+export const HeaderTemplate = {
+  render: (args) => <Header2></Header2>,
+};
